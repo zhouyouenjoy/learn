a10492168fdaa03379e093a563113dbe7cb32a38
unknown <zhouyoubnu@gmail.com~>
2025-04-23 10:54:34 +0800

    第六次提交

---
+++ 围栏/右围栏.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+func _physics_process(delta: float) -> void:
+	for i in get_overlapping_areas():
+			if i.is_in_group("ball"):
+				i.rest()
+	pass
--- 小球/小球.gd
+++ 小球/小球.gd
@@ -1,7 +1,7 @@
 extends Area2D
 var init_position
 var x=5
-var y=-5
+var y=5
 var vec=Vector2(x,y)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,14 +11,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move()
-	rest()
 	
 func move():
 	position=position+vec
 		
 	pass
 func rest():
-	if position.x>1120 or position.x<0:
+		if vec.x>0:
+			ScoreCount.score1+=1
+		else :
+			ScoreCount.score2+=1
 		vec = Vector2(x,y)
 		position = init_position
 	
